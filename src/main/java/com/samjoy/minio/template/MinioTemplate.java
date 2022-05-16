4ffb7aad147f17e6c9e08fdd4451288b8d35095c
Wenjie Li <2077769658@qq.com>
2022-05-16 12:44:59 +0800

    添加 hutool 工具包,template添加上传、删除、获取等方法

--- src/main/java/com/samjoy/minio/template/MinioTemplate.java
+++ src/main/java/com/samjoy/minio/template/MinioTemplate.java
@@ -2,7 +2,10 @@ package com.samjoy.minio.template;
 
 import io.minio.errors.MinioException;
 import io.minio.messages.Bucket;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -18,6 +21,7 @@ public interface MinioTemplate {
      *
      * @param bucketName 桶名称
      * @return true：bucket存在，false：bucket不存在
+     * @throws MinioException
      */
     Boolean bucketExists(String bucketName) throws MinioException;
 
@@ -26,6 +30,7 @@ public interface MinioTemplate {
      * 列出所有的桶
      *
      * @return 桶的列表
+     * @throws MinioException
      */
     List<Bucket> listBuckets() throws MinioException;
 
@@ -34,7 +39,87 @@ public interface MinioTemplate {
      *
      * @param bucketName 桶名称
      * @return true：创建新桶成功，false：创建的桶已经存在存在
+     * @throws MinioException
      */
     Boolean createBucket(String bucketName) throws MinioException;
 
+    /**
+     * 上传文件到bucket
+     *
+     * @param file 需要上传文件
+     * @return 保存的文件名
+     * @throws MinioException
+     */
+    String putFile(File file) throws MinioException;
+
+    /**
+     * 上传文件到bucket
+     *
+     * @param objectName 自定义文件名
+     * @param file       需要上传的文件
+     * @return 实际保存的文件名
+     * @throws MinioException
+     */
+    String putFile(String objectName, File file) throws MinioException;
+
+    /**
+     * 上传MultipartFile文件到bucket
+     *
+     * @param multipartFile 需要上传的multipartFile
+     * @return 实际保存的文件名
+     * @throws MinioException
+     */
+    String putMultipartFile(MultipartFile multipartFile) throws MinioException;
+
+    /**
+     * 上传MultipartFile文件到bucket
+     *
+     * @param objectName    自定义文件名
+     * @param multipartFile 需要上传的multipartFile
+     * @return 实际保存的文件名
+     * @throws MinioException
+     */
+    String putMultipartFile(String objectName, MultipartFile multipartFile) throws MinioException;
+
+    /**
+     * 使用流的方式保存文件名
+     *
+     * @param objectName  自定义文件名
+     * @param inputStream 输入流
+     * @param contentType 文件类型
+     * @throws MinioException
+     */
+    void putObject(String objectName, InputStream inputStream, String contentType) throws MinioException;
+
+    /**
+     * 根据文件名删除文件
+     *
+     * @param objectName
+     * @throws MinioException
+     */
+    void removeObject(String objectName) throws MinioException;
+
+    /**
+     * 根据文件名获取文件的输出流
+     *
+     * @param objectName 文件名
+     * @throws MinioException
+     */
+    InputStream getObject(String objectName) throws MinioException;
+
+    /**
+     * 根据URL获取文件的输出流
+     *
+     * @param url 文件的网络路径
+     * @throws MinioException
+     */
+    InputStream getObjectByUrl(String url) throws MinioException;
+
+    /**
+     * 根据文件名获取真实的文件名称
+     *
+     * @param objectName 文件名称
+     * @return 真实的文件名称
+     */
+    String getTrueFileName(String objectName);
 }
--- src/main/java/com/samjoy/minio/template/impl/MinioTemplateImpl.java
+++ src/main/java/com/samjoy/minio/template/impl/MinioTemplateImpl.java
@@ -1,16 +1,24 @@
 package com.samjoy.minio.template.impl;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.text.StrPool;
+import cn.hutool.core.util.IdUtil;
+import com.samjoy.minio.MinioProperties;
 import com.samjoy.minio.template.MinioTemplate;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
+import io.minio.*;
 import io.minio.errors.MinioException;
 import io.minio.messages.Bucket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.activation.MimetypesFileTypeMap;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -29,6 +37,9 @@ public class MinioTemplateImpl implements MinioTemplate {
 
     @Autowired
     private MinioClient minioClient;
+
+    @Autowired
+    private MinioProperties minioProperties;
 
     @Override
     public Boolean bucketExists(String bucketName) throws MinioException {
@@ -59,6 +70,7 @@ public class MinioTemplateImpl implements MinioTemplate {
             } else {
                 // 桶不存在创建桶，并返回true
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                log.info("创建桶：{} 成功", bucketName);
                 return true;
             }
         } catch (Exception e) {
@@ -66,5 +78,92 @@ public class MinioTemplateImpl implements MinioTemplate {
         }
     }
 
+    @Override
+    public String putFile(File file) throws MinioException {
+        return this.putFile(file.getName(), file);
+    }
 
+    @Override
+    public String putFile(String objectName, File file) throws MinioException {
+        String fileName = getFileNameWithSnowflakeId(objectName);
+        try {
+            this.putObject(fileName, FileUtil.getInputStream(file), new MimetypesFileTypeMap().getContentType(file));
+        } catch (Exception e) {
+            throw new MinioException("上传失败");
+        }
+        return fileName;
+    }
+
+    @Override
+    public String putMultipartFile(MultipartFile multipartFile) throws MinioException {
+        return this.putMultipartFile(multipartFile.getName(), multipartFile);
+    }
+
+    @Override
+    public String putMultipartFile(String objectName, MultipartFile multipartFile) throws MinioException {
+        String fileName = getFileNameWithSnowflakeId(objectName);
+        try {
+            this.putObject(fileName, multipartFile.getInputStream(), multipartFile.getContentType());
+        } catch (Exception e) {
+            throw new MinioException("上传失败");
+        }
+        return fileName;
+    }
+
+    @Override
+    public void putObject(String objectName, InputStream inputStream, String contentType) throws MinioException {
+        try {
+            minioClient.putObject(PutObjectArgs.builder().bucket(minioProperties.getBucket()).object(objectName).stream(
+                            inputStream, inputStream.available(), -1)
+                    .contentType(contentType)
+                    .build());
+            log.info("上传文件 {} 到 桶：{}成功", objectName, minioProperties.getBucket());
+        } catch (Exception e) {
+            throw new MinioException(String.format("上传文件%s 到 {bucket} %s 失败!", objectName, minioProperties.getBucket()));
+        }
+    }
+
+    @Override
+    public void removeObject(String objectName) throws MinioException {
+        try {
+            minioClient.removeObject(RemoveObjectArgs.builder().bucket(minioProperties.getBucket()).object(objectName).build());
+            log.info("删除桶：{}下的 {}成功", minioProperties.getBucket(), objectName);
+        } catch (Exception e) {
+            throw new MinioException(String.format("删除文件 %s 失败!", objectName));
+        }
+    }
+
+    @Override
+    public InputStream getObject(String objectName) throws MinioException {
+        try {
+            return minioClient.getObject(GetObjectArgs.builder().bucket(minioProperties.getBucket()).object(objectName).build());
+        } catch (Exception e) {
+            throw new MinioException("根据文件名获取流失败!");
+        }
+    }
+
+    @Override
+    public InputStream getObjectByUrl(String url) throws MinioException {
+        try {
+            return new URL(url).openStream();
+        } catch (IOException e) {
+            throw new MinioException("根据URL获取流失败!");
+        }
+    }
+
+    /**
+     * 使用雪花算法，防止文件重名
+     *
+     * @param fileName 文件名
+     * @return 使用雪花算法加上文件后缀名，保证文件名不会重复
+     */
+    private String getFileNameWithSnowflakeId(String fileName) {
+        return FileUtil.getPrefix(fileName) + StrPool.DASHED + IdUtil.getSnowflake().nextIdStr() + StrPool.DOT + FileUtil.getSuffix(fileName);
+    }
+
+    @Override
+    public String getTrueFileName(String objectName) {
+        String prefix = FileUtil.getPrefix(objectName);
+        return prefix.substring(0, objectName.length() - 20) + StrPool.DOT + FileUtil.getSuffix(objectName);
+    }
 }
